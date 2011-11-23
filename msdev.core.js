c5d44169b30239c5d0540c687af821c5948f670a
Carlos Abdalla <abdalla@msdev.com.br>
2011-11-23 17:00:26 -0200

    Adicionado mais alguns metodos

--- msdev.core.js
+++ msdev.core.js
@@ -62,3 +62,38 @@ Array.matrix = function (x, y, initial){
 //Ex: var matrixXpto = Array.matrix(4, 4, 0);
 //    alert(matrixXpto[1][2]); ==> 0
 
+Array.method('pop', function(){
+	return this.splice(this.lenght - 1, 1)[0];
+});
+// Ex: var a = ['a','b','c'];
+//     var b = a.pop(); ==> a ficará ['a','b'] e b ficará 'c'
+
+Array.method('shift', function(){
+	return this.splice(0,1)[0];
+});
+// Ex: var a = ['a','b','c'];
+//     var b = a.shift(); ==> a ficará ['b','c'] e b ficará 'a'
+
+Array.method('push', function(){
+	this.splice.apply(
+		this, 
+		[this.length, 0].concat(Array.prototype.slice.apply(arguments))
+	);
+	
+	return this.lenght;
+});
+// Ex: var a = ['a','b','c'];
+//     var b = ['x','y','z'];
+//     var c = a.push(b, true); ==>  a ficará ['a','b','c', ['x','y','z'], true] e c ficará 5
+rray.method('unshift', function(){
+	this.splice.apply(
+		this, 
+		[0, 0].concat(Array.prototype.slice.apply(arguments))
+	);
+	
+	return this.lenght;
+});
+// Ex: var a = ['a','b','c'];
+//     var c = a.unshift('x','y'), true); ==>  a ficará ['x','y', 'a','b','c'] e c ficará 5
+
+
